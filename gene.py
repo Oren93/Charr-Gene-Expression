323138c39e5bc8e5c320f0fff2f129aa56ed6c3a
Oren93 <54531718+Oren93@users.noreply.github.com>
2021-09-10 01:58:24 +0000

    complete report

---
+++ gene.py
@@ -0,0 +1,141 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import classification_report
+from sklearn.metrics import precision_score
+from sklearn.model_selection import train_test_split
+from collections import Counter
+import matplotlib.pyplot as plt
+import seaborn as sn
+import pandas as pd
+import numpy as np
+import predict
+import os
+
+# ararnge the data such that gene locations are indexed for each cheomosome, can also add a column of index as fraction of the whole 
+def arrange_by_index(df,col='index',newcol=None):
+    i = 2
+    max_index = []  # how many observations per chromosome
+    j=0
+    for j in range(1, len(df)):
+        if df['chromosome'][j] != df['chromosome'][j - 1]:
+            max_index.append(i-1)
+            i = 1
+        df.loc[j, ['index']] = [i]
+        i += 1
+    #    j += 1
+    max_index.append(i-1)
+    df.loc[0, ['index']] = [1] # index now represents the relative location of the gene of it's chromosome
+
+    if newcol is None: 
+        return df
+    i = 0
+    df.loc[0, [newcol]] = [df[col][0] / max_index[0]]
+    for j in range(1, len(df)):
+        if df['chromosome'][j] != df['chromosome'][j - 1]:
+            i += 1
+        df.loc[j, [newcol]] = [df[col][j] / max_index[i]]
+    return df
+
+
+# input: data frame of genes locations on chromosomes
+# output: the dataframe with added column of the gap on the chromosome where gene isn't mapped
+#         works on the dataframe itself, not on a copy
+def calc_unknown_gap(df, colname = 'gap'):
+    for j in range(1, len(df)):
+        if df['chromosome'][j] != df['chromosome'][j - 1]:
+            df.loc[j, [colname]] = df['start'][j]
+        else:
+            tmp = df['start'][j] - df['end'][j-1]
+            if tmp < 0: tmp = 0
+            df.loc[j, [colname]] = tmp
+    df.loc[0, [colname]] = df['start'][0]
+    return df
+
+
+# ararnge the data such that gene locations are fractions of the chromosome
+def gene_as_fraction(data,col,newcol):
+    df = data.copy()
+    length = []
+    for j in range(1, len(df)):
+        if j == len(df) - 1 or df['chromosome'][j] != df['chromosome'][j + 1]:
+            length.append(df['end'][j])
+
+    i = 0
+    df.loc[0, [newcol]] = [df[col][0] / length[0]]
+    for j in range(1, len(df)):
+        if df['chromosome'][j] != df['chromosome'][j - 1]:
+            i += 1
+        df.loc[j, [newcol]] = [df[col][j] / length[i]]
+    return df
+
+def split_train_test_draft(df, percent):
+    train, test = train_test_split(df, train_size=percent, random_state=None, shuffle=True, stratify=None)
+    train_df = {}
+    test_df = {}
+    chromosomes = df['chromosome'].unique()
+    for crm in chromosomes:
+        train_df[crm] = train.loc[train['chromosome'] == crm].drop(['chromosome'], axis=1)
+        test_df[crm] = test.loc[test['chromosome'] == crm].drop(['chromosome'], axis=1)
+        test.loc[test['chromosome'] == crm].drop(['chromosome'], axis=1)
+    return train, test, train_df, test_df
+
+
+# add columns of different clusters by method, the file name is expected to
+#  contain the number of clusters first followes by '_'
+def add_clusters(files_list, df, method):
+    ## Merge original table with other tables to add other clustering columns
+    data = df.copy()
+    for file in files_list:
+        ndf = pd.read_csv(file)[['gene', 'cluster']]
+        name = os.path.basename(file)
+        name = name.split(".")[0]
+        header = name.split("_")
+        data = pd.merge(
+            data,
+            ndf,
+            how="left",
+            on=['gene'],
+            #left_index=True,
+            #right_index=False,
+            sort=False,
+            suffixes=("", f'_{header[0]}_{method}'),
+            copy=True,
+            indicator=False,
+            validate=None,
+        )
+    return data
+
+# counts how any genes sticked together in all clustering methods
+def count_loyal_genes(data ,method = ''):
+    columns = []
+    df = data.copy()
+    if method == '':
+        for col in df.columns:
+            if 'cluster_' in col:
+                columns.extend([col])
+    else:
+        for col in df.columns:
+            if f'_{method}' in col:
+                columns.extend([col])
+    df = data[['gene']+columns]
+    #
+    df['factorize'] = 0
+    prime = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+             67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137,
+             139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211,
+             223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283,
+             293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373,379,
+             383, 389, 397, 401]
+    i = 0
+    for col in columns:
+        df['factorize'] = df['factorize'] + df[col] *prime[i]
+        i = i + 1
+    return(Counter(df['factorize']))
+   
+
+
+
+
+
+
+
---
+++ predict.py
@@ -0,0 +1,172 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from sklearn.metrics import precision_score
+from sklearn.metrics import f1_score
+import matplotlib.pyplot as plt
+
+
+# splitting into two types of train and test, one type is a split over the whole data frame and the other is to dictionaries of 
+# train and test sets for each chromosome
+def split_train_test(df, percent = 0.75):
+    train, test = train_test_split(df, train_size=percent, random_state=None, shuffle=True, stratify=None)
+    train_df = {}
+    test_df = {}
+    chromosomes = df['chromosome'].unique()
+    for crm in chromosomes:
+        train_df[crm] = train.loc[train['chromosome'] == crm].drop(['chromosome'], axis=1)
+        test_df[crm] = test.loc[test['chromosome'] == crm].drop(['chromosome'], axis=1)
+        test.loc[test['chromosome'] == crm].drop(['chromosome'], axis=1)
+    return train, test, train_df, test_df
+
+# train model 1 means 1 training of the whole data, 2 means to train for each chromosome
+def eval_predictions(data, features, target, train_model = 1,percent = 0.75):
+    tmp = data[features+['chromosome'] + [target]] # no need the whole table
+    train,test,train_df,test_df = split_train_test(tmp,percent)
+    predictions = {}
+    actual = []  # a list of expected data, better to define here in case we shuffle the original table
+    clf = RandomForestClassifier(n_jobs=2, random_state=False, warm_start=False)
+    if train_model == 1:
+        clf.fit(train[features], train[target])
+        for key in test_df.keys():
+            actual.extend(list(test_df[key][target]))
+            predictions[key] = clf.predict(test_df[key][features])
+            
+    if train_model == 2:
+        for key in train_df.keys():
+            clf.fit(train_df[key][features], train_df[key][target])
+            predictions[key] = clf.predict(test_df[key][features])
+    ## making confusion matrices
+    total_predictions = []  # collect the prediction lists of method 1 (one training model to all tests) to one list
+    for arr in predictions.values():
+        total_predictions.extend(arr)
+    CM = confusion_matrix(test[target], total_predictions)  # confusion matrix of all chromosomes
+    return test[target], total_predictions, CM
+
+# input: dataframe, clustering method name as appear at column names, range number of clusters (including both ends)
+# output: dictionary of actual values, predictionsm and comfusion matrices
+def analyze(df,features = ['index', 'length'] ,method = 'pam',clusterRange = [2,26],percent = 0.75, iterations=1,train_model=1):
+    CM = {}
+    actualVals, pred = {}, {}
+    print('|', ' ' * (clusterRange[1]-clusterRange[0]+1), '|100%\n|', sep="", end="")
+    for i in range(clusterRange[0],clusterRange[1]+1):
+        max_f1_score = 0
+        for iters in range(0,iterations):
+            j = i if i > 9 else f'0{i}'
+            header = f'cluster_{j}_{method}'
+            actual,predictions,cm = eval_predictions(df, features, target=header,train_model=train_model,percent=percent)
+            f1 = f1_score(actual, predictions,labels=None, pos_label=1, average='micro')
+            if f1 > max_f1_score:
+                actualVals[i] = actual
+                pred[i] = predictions
+                CM[i] = cm#confusion_matrix(actual, predictions)
+        print('-', end="")
+    print('|')
+    return actualVals,pred, CM
+
+
+# trying to predict a whole chromosome based on other chromosomes
+def predict2(df,method,features,clusterRange = [2,26]):
+    predictions = {}
+    vals = {}
+    CM = {}
+    chro = df['chromosome'].unique()
+    for i in range(clusterRange[0],clusterRange[1]+1):
+        preds = {}
+        j = i if i > 9 else f'0{i}'
+        header = f'cluster_{j}_{method}'
+        for chr in chro:
+            train,test = df.loc[df['chromosome'] != chr],df.loc[df['chromosome'] == chr]
+            clf = RandomForestClassifier(n_jobs=2, random_state=False, warm_start=False)
+            clf.fit(train[features], train[header])
+            preds[chr] = clf.predict(test[features])
+    ## making confusion matrices
+        total_predictions = []  # collect the prediction lists of method 1 (one training model to all tests) to one list
+        for arr in preds.values():
+            total_predictions.extend(arr)
+        predictions[i] = total_predictions
+        vals[i] = df[header]
+        CM[i] = confusion_matrix(df[header], total_predictions)  # confusion matrix of all chromosomes
+    return vals,predictions,CM
+
+# WARNING: Extremely slow algorithm
+# trying to predict a one gene on the chromosome based on all other genes in the chromosome
+# The method is known as 'Leave One Out'
+def predict3(df,method,features,clusterRange = [2,26]):
+    predictions = {}
+    vals = {}
+    CM = {}
+    chro = df['chromosome'].unique()
+    for i in range(clusterRange[0],clusterRange[1]+1):
+        j = i if i > 9 else f'0{i}'
+        header = f'cluster_{j}_{method}'
+        preds = {}
+        for chr in chro:
+            crmDf = df.loc[df['chromosome'] == chr]
+            predicted_cluster = []
+            for index in crmDf.index:
+                train,test = df[df.index!=index],df[df.index==index]
+                clf = RandomForestClassifier(n_jobs=6, random_state=False, warm_start=False)
+                clf.fit(train[features], train[header])
+                predicted_cluster.append(clf.predict(test[features]))
+            preds[chr] = predicted_cluster
+    ## making confusion matrices
+        total_predictions = [] 
+        for arr in preds.values():
+            total_predictions.extend(arr)
+        predictions[i] = total_predictions
+        vals[i] = df[header]
+        CM[i] = confusion_matrix(df[header], total_predictions)  # confusion matrix of all chromosomes
+    return vals,predictions,CM
+
+# Plotting the prediction score graph of PAM clustering from 2 to 26 clusters
+def plotit(vals, preds,title = 'predicion score',score = 'precision',avg='micro',multiplot=False,colour=None):
+    if not multiplot:
+        minClusters = min(vals.keys()) ## minimum number of clusters 
+        x = []
+        if score == 'precision':
+            for i in range(minClusters,int(len(vals)+minClusters)):
+                x = x +[precision_score(vals[i], preds[i], labels=None, pos_label=1, average=avg, sample_weight=None, zero_division='warn')]
+        elif score == 'f1':
+            for i in range(minClusters,int(len(vals)+minClusters)):
+                x = x +[f1_score(vals[i], preds[i],labels=None, pos_label=1, average=avg)]
+        else: 
+            print('invalid scoring method')
+        if colour is None:
+            plt.plot(range(minClusters, len(x)+minClusters),x)
+        else:
+            plt.plot(range(minClusters, len(x)+minClusters),x,color=colour)            
+    if multiplot:
+        minClusters = min(list(vals[list(vals.keys())[0]].keys())) ## minimum number of clusters 
+        di = {}
+        for i in vals.keys():
+            x = []
+            if score == 'precision':
+                for j in range(minClusters,int(len(vals[i])+minClusters)):
+                    x = x +[precision_score(vals[i][j], preds[i][j], labels=None, pos_label=1, average=avg, sample_weight=None, zero_division='warn')]
+                di[i] = x
+            elif score == 'f1':
+                for j in range(minClusters,int(len(vals[i])+minClusters)):
+                    x = x +[f1_score(vals[i][j], preds[i][j],labels=None, pos_label=1, average=avg)]
+                di[i] = x
+            else: print('invalid scoring method')
+            if colour is None:
+                plt.plot(range(minClusters, len(x)+minClusters),x)
+            else: 
+                plt.plot(range(minClusters, len(x)+minClusters),x,color=colour)
+    plt.title(title)
+    plt.xlabel('Number of clusters')
+    plt.ylabel('precision')
+    plt.grid()
+    plt.show()
+
+    
+# Plot confusion matrix
+def plotCM(CM,title,dpi = 500,fontSize = 20):
+    plt.rcParams.update({'font.size': fontSize})
+    classNames = range(1,len(CM)+1)
+    disp = ConfusionMatrixDisplay(confusion_matrix=CM,display_labels=classNames)
+    disp.plot(include_values=True, cmap='Blues', ax=None, xticks_rotation='horizontal')
+    plt.gcf().set_dpi(dpi)
+    plt.title(title)
+    plt.show()
