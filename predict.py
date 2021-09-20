9a2c4afaec15c286a8ff9d7e0718f3aff011e031
Oren93 <54531718+Oren93@users.noreply.github.com>
2021-09-20 00:56:02 +0000

    bug fix

--- predict.py
+++ predict.py
@@ -20,12 +20,12 @@ def split_train_test(df, percent = 0.75):
     return train, test, train_df, test_df
 
 # train model 1 means 1 training of the whole data, 2 means to train for each chromosome
-def eval_predictions(data, features, target, train_model = 1,percent = 0.75):
+def eval_predictions(data, features, target, train_model = 1,percent = 0.75,cores=-1,class_weight=None):
     tmp = data[features+['chromosome'] + [target]] # no need the whole table
     train,test,train_df,test_df = split_train_test(tmp,percent)
     predictions = {}
     actual = []  # a list of expected data, better to define here in case we shuffle the original table
-    clf = RandomForestClassifier(n_jobs=2, random_state=False, warm_start=False)
+    clf = RandomForestClassifier(n_jobs=cores, random_state=False, warm_start=False,class_weight=class_weight)
     if train_model == 1:
         clf.fit(train[features], train[target])
         for key in test_df.keys():
@@ -45,7 +45,7 @@ def eval_predictions(data, features, target, train_model = 1,percent = 0.75):
 
 # input: dataframe, clustering method name as appear at column names, range number of clusters (including both ends)
 # output: dictionary of actual values, predictionsm and comfusion matrices
-def analyze(df,features = ['index', 'length'] ,method = 'pam',clusterRange = [2,26],percent = 0.75, iterations=1,train_model=1):
+def analyze(df,features = ['index', 'length'] ,method = 'pam',clusterRange = [2,26],percent = 0.75, iterations=1,train_model=1,cores=-1):
     CM = {}
     actualVals, pred = {}, {}
     print('|', ' ' * (clusterRange[1]-clusterRange[0]+1), '|100%\n|', sep="", end="")
@@ -60,13 +60,14 @@ def analyze(df,features = ['index', 'length'] ,method = 'pam',clusterRange = [2,
                 actualVals[i] = actual
                 pred[i] = predictions
                 CM[i] = cm#confusion_matrix(actual, predictions)
+                max_f1_score = f1
         print('-', end="")
     print('|')
     return actualVals,pred, CM
 
 
 # trying to predict a whole chromosome based on other chromosomes
-def predict2(df,method,features,clusterRange = [2,26]):
+def predict2(df,method,features,clusterRange = [2,26],cores=-1):
     predictions = {}
     vals = {}
     CM = {}
@@ -77,7 +78,7 @@ def predict2(df,method,features,clusterRange = [2,26]):
         header = f'cluster_{j}_{method}'
         for chr in chro:
             train,test = df.loc[df['chromosome'] != chr],df.loc[df['chromosome'] == chr]
-            clf = RandomForestClassifier(n_jobs=2, random_state=False, warm_start=False)
+            clf = RandomForestClassifier(n_jobs=cores, random_state=False, warm_start=False)
             clf.fit(train[features], train[header])
             preds[chr] = clf.predict(test[features])
     ## making confusion matrices
@@ -87,6 +88,23 @@ def predict2(df,method,features,clusterRange = [2,26]):
         predictions[i] = total_predictions
         vals[i] = df[header]
         CM[i] = confusion_matrix(df[header], total_predictions)  # confusion matrix of all chromosomes
+    return vals,predictions,CM
+
+# trying to predict a whole chromosome based on other chromosomes, works for a single target column
+def predict4(df,target,features,cores=-1):
+    chro = df['chromosome'].unique()
+    preds = {}
+    for chr in chro:
+        train,test = df.loc[df['chromosome'] != chr],df.loc[df['chromosome'] == chr]
+        clf = RandomForestClassifier(n_jobs=cores, random_state=False, warm_start=False)
+        clf.fit(train[features], train[target])
+        preds[chr] = clf.predict(test[features])
+    ## making confusion matrices
+    predictions = []  # collect the prediction lists of method 1 (one training model to all tests) to one list
+    for arr in preds.values():
+        predictions.extend(arr)
+    vals = df[target]
+    CM = confusion_matrix(df[target], predictions)  # confusion matrix of all chromosomes
     return vals,predictions,CM
 
 # WARNING: Extremely slow algorithm
@@ -119,8 +137,33 @@ def predict3(df,method,features,clusterRange = [2,26]):
         CM[i] = confusion_matrix(df[header], total_predictions)  # confusion matrix of all chromosomes
     return vals,predictions,CM
 
+
+# WARNING: Leave one out, single target column
+def predict5(df,target,features,cores=-1):
+    predictions = {}
+    vals = {}
+    CM = {}
+    chro = df['chromosome'].unique()
+    preds = {}
+    for chr in chro:
+        crmDf = df.loc[df['chromosome'] == chr]
+        predicted_cluster = []
+        for index in crmDf.index:
+            train,test = df[df.index!=index],df[df.index==index]
+            clf = RandomForestClassifier(n_jobs=cores, random_state=False, warm_start=False)
+            clf.fit(train[features], train[target])
+            predicted_cluster.append(clf.predict(test[features]))
+        preds[chr] = predicted_cluster
+    ## making confusion matrices
+    total_predictions = [] 
+    for arr in preds.values():
+        total_predictions.extend(arr)
+    CM = confusion_matrix(df[target], total_predictions)  # confusion matrix of all chromosomes
+    return df[target],total_predictions,CM
+
+
 # Plotting the prediction score graph of PAM clustering from 2 to 26 clusters
-def plotit(vals, preds,title = 'predicion score',score = 'precision',avg='micro',multiplot=False,colour=None):
+def plotit(vals, preds,title = 'predicion score',score = 'precision',avg='micro',multiplot=False,colour=None,labels=False):
     if not multiplot:
         minClusters = min(vals.keys()) ## minimum number of clusters 
         x = []
@@ -151,14 +194,23 @@ def plotit(vals, preds,title = 'predicion score',score = 'precision',avg='micro'
                 di[i] = x
             else: print('invalid scoring method')
             if colour is None:
-                plt.plot(range(minClusters, len(x)+minClusters),x)
-            else: 
-                plt.plot(range(minClusters, len(x)+minClusters),x,color=colour)
+                if not labels:
+                    plt.plot(range(minClusters, len(x)+minClusters),x)
+                else:
+                    plt.plot(range(minClusters, len(x)+minClusters),x,label=labels[i])
+            else:
+                if not labels: 
+                    plt.plot(range(minClusters, len(x)+minClusters),x,color=colour)
+                else:
+                    plt.plot(range(minClusters, len(x)+minClusters),x,color=colour)
     plt.title(title)
+    if labels:
+        plt.legend()
     plt.xlabel('Number of clusters')
     plt.ylabel('precision')
     plt.grid()
     plt.show()
+
 
     
 # Plot confusion matrix
